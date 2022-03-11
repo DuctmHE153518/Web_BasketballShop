2cfb93a8cf161d8afb181cf17dcdcf8d6592f17d
DuctmHE153518 <ductmhe153518@fpt.edu.vn>
2022-03-12 00:50:23 +0700

    ud

--- WebBasketballShop/test/test.java
+++ WebBasketballShop/test/test.java
@@ -9,16 +9,16 @@ import model.Product;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Duc Tran
  */
 public class test {
-        public static void main(String[] args) {
+
+    public static void main(String[] args) {
         ShopDAO dao = new ShopDAO();
         ArrayList<Product> list = dao.getTrend();
-        for(Product o : list) {
+        for (Product o : list) {
             System.out.println(o);
         }
     }
